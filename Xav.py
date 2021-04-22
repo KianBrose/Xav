239f14ee8b4be71f2104f9c63e653cadd1d1aab7
Skyoathzero <73295100+Skyoathzero@users.noreply.github.com>
2021-04-22 19:00:25 +0700

    Added a cog support for further development of the bot
    added a cog loader system for the bot

--- Xav.py
+++ Xav.py
@@ -1,17 +1,23 @@
 import discord
 from discord.ext import commands    
 bot = commands.Bot(command_prefix='€')
+
+for filename in os.listdir('./Cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'Cogs.{filename[:-3]}')
+    else: pass
+
 @bot.command()
 async def info(ctx):
     texttosend = """
     -Ask SPECIFIC questions, don't expect help if you say "It doesn't work" "Why isn't it working" "can you fix it for me" Instead, describe the problem/s, the steps you tried to fix it, the objective etc, don't make people dig infornation out of you
-                 """
+                """
     await ctx.send(texttosend)
 @bot.command()
 async def tos(ctx):
     texttosend = """
     - As much as I don't like discord TOS, discussion of cheating or automation of games is not allowed
-                 """
+                """
     await ctx.send(texttosend)
 @bot.command()
 async def w(ctx):
