b3824a13600ebfb147127c0026ee55dfbac7279a
Kian Brose <62078964+KianBrose@users.noreply.github.com>
2021-04-22 10:40:24 +0200

    Uploaded main code
    It ain't much but it does what it's made to do well xD

---
+++ Xav.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands    
+bot = commands.Bot(command_prefix='€')
+@bot.command()
+async def info(ctx):
+    texttosend = """
+    -Ask SPECIFIC questions, don't expect help if you say "It doesn't work" "Why isn't it working" "can you fix it for me" Instead, describe the problem/s, the steps you tried to fix it, the objective etc, don't make people dig infornation out of you
+                 """
+    await ctx.send(texttosend)
+@bot.command()
+async def tos(ctx):
+    texttosend = """
+    - As much as I don't like discord TOS, discussion of cheating or automation of games is not allowed
+                 """
+    await ctx.send(texttosend)
+@bot.command()
+async def w(ctx):
+    await ctx.send("Welcome!")
+bot.run('')
