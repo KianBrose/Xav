855300e4b194286bc6e5fb2a4e2e32387baa7584
GammaRay99 <52253862+GammaRay99@users.noreply.github.com>
2021-07-01 12:46:01 +0200

    Update Xav.py

--- Xav.py
+++ Xav.py
@@ -1,20 +1,57 @@
 import discord
 from discord.ext import commands
 import random
+import os
 
+import db
 from utils import Guild_utils
 
 
-
-GUILD_ID = 711325947269349448
+GUILD_ID = 700300301030391828
 
 bot = commands.Bot(command_prefix='€')
 guild = Guild_utils(bot, GUILD_ID)
+db.init()
+
+def register_member(member):
+    id = member.id
+    name = member.name
+    roles = list(map(lambda r: r.id, member.roles))
+    db.add_member(id, name, roles)
 
 
 @bot.event
 async def on_ready():
     print(f"Logged as {bot.user}")
+
+
+@bot.event
+async def on_message(message):
+    if message.author.bot or message.channel.type == discord.ChannelType.private:
+        return
+    
+    # If the author is new, we register him
+    if db.get_member(message.author.id) is None:
+        register_member(message.author)
+    
+    # karma gestion
+    if message.content.lower().split(" ")[0] in {"thx", "thanks", "ty"}:
+        if message.mentions:
+            for member in message.mentions:
+                if db.get_member(member.id) is None:
+                    register_member(member)
+                
+                if member.id == message.author.id:
+                    await message.channel.send("nerd")
+                    return
+                if member.bot:
+                    await message.channel.send("bruh")
+                    return
+                
+                db.update_member_karma(member.id, 1)
+                await message.add_reaction("👍")
+
+    await bot.process_commands(message)
 
 
 @bot.command()
@@ -35,7 +72,16 @@ async def tos(ctx):
 
 @bot.command()
 async def w(ctx):
-    await ctx.send(random.choices(['Welcome', 'Welcum'], [0.99, 0.01]))  # 1% chance of saying "Welcum"
+    await ctx.send(random.choices(['Welcome', 'Welcum'], [0.99, 0.01])[0])  # 1% chance of saying "Welcum"
 
 
-bot.run()
+@bot.command()
+async def money(ctx):
+    await ctx.send("https://www.youtube.com/watch?v=KigVdcSr8s4")
+
+@bot.command()
+async def karma(ctx):
+    await ctx.send(f"You have : {db.get_member(ctx.author.id)['karma']} karmas")
+
+
+bot.run(os.environ["TOKEN"])
