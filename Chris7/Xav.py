fe7343abd8df8631840458590febbd02c0a30941
ChrisCh7 <42211808+ChrisCh7@users.noreply.github.com>
2021-07-27 03:30:03 +0300

    Add ghost ping detector

---
+++ Chris7/Xav.py
@@ -0,0 +1,36 @@
+from discord.ext import commands
+
+from ghost_ping_detector import GhostPingDetector
+
+bot = commands.Bot(command_prefix='€', case_insensitive=True)
+
+bot.add_cog(GhostPingDetector(bot))
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged as XAV")
+
+
+@bot.command()
+async def info(ctx):
+    texttosend = """
+    -Ask SPECIFIC questions, don't expect help if you say "It doesn't work" "Why isn't it working" "can you fix it for me" Instead, describe the problem/s, the steps you tried to fix it, the objective etc, don't make people dig infornation out of you
+                 """
+    await ctx.send(texttosend)
+
+
+@bot.command()
+async def tos(ctx):
+    texttosend = """
+    - As much as I don't like discord TOS, discussion of cheating or automation of games is not allowed
+                 """
+    await ctx.send(texttosend)
+
+
+@bot.command()
+async def w(ctx):
+    await ctx.send('Welcome')
+
+
+bot.run("")
